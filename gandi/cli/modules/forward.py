a8c84e6bac1faf824cb010a3ce9996905cd29bb2
Grigouze <grigouze@yahoo.fr>
2020-12-29 00:42:25 +0100

    add forward

--- gandi/cli/modules/forward.py
+++ gandi/cli/modules/forward.py
@@ -1,4 +1,5 @@
 """ Forward commands module. """
+import json
 
 from gandi.cli.core.base import GandiModule
 
@@ -14,14 +15,31 @@ class Forward(GandiModule):
 
     """
 
+    api_url = 'https://api.gandi.net/v5/email'
+
     @classmethod
     def list(cls, domain, options):
         """List forwards for a given domain name."""
+
+        if cls.get('apirest.key'):
+            if 'items_per_page' in options:
+                options = '?per_page=%s' % options['items_per_page']
+            else:
+                options = ''
+
+            return cls.json_get('%s/forwards/%s%s'
+                                % (cls.api_url, domain, options))
+
         return cls.call('domain.forward.list', domain, options)
 
     @classmethod
     def delete(cls, domain, source):
         """Delete a domain mail forward."""
+
+        if cls.get('apirest.key'):
+            return cls.json_delete('%s/forwards/%s/%s'
+                                   % (cls.api_url, domain, source))
+
         return cls.call('domain.forward.delete', domain, source)
 
     @classmethod
@@ -29,6 +47,13 @@ class Forward(GandiModule):
         """Create a domain mail forward."""
         cls.echo('Creating mail forward %s@%s' % (source, domain))
         options = {'destinations': list(destinations)}
+
+        if cls.get('apirest.key'):
+            options['source'] = source
+            return cls.json_post('%s/forwards/%s'
+                                 % (cls.api_url, domain),
+                                 data=json.dumps(options))
+
         result = cls.call('domain.forward.create', domain, source, options)
 
         return result
@@ -64,7 +89,13 @@ class Forward(GandiModule):
                     or (current_destinations != fwds)):
                 cls.echo('Updating mail forward %s@%s' % (source, domain))
                 options = {'destinations': fwds}
-                result = cls.call('domain.forward.update', domain, source,
-                                  options)
+
+                if cls.get('apirest.key'):
+                    result = cls.json_put('%s/forwards/%s/%s'
+                                          % (cls.api_url, domain, source),
+                                          data=json.dumps(options))
+                else:
+                    result = cls.call('domain.forward.update', domain, source,
+                                      options)
 
         return result
